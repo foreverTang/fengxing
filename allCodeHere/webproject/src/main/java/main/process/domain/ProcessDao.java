971741b5fa9ad8eeaf0374c590fadcaeb9c71a30
foreverTang <forever9@foxmail.com>
2015-12-10 09:28:09 +0800

    登陆和主处理后台
    Signed-off-by: foreverTang <forever9@foxmail.com>

---
+++ allCodeHere/webproject/src/main/java/main/process/domain/ProcessDao.java
@@ -0,0 +1,44 @@
+package main.process.domain;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+//import javax.mail.Flags.Flag;
+import main.process.service.ProcessService;
+import main.process.service.ProcessService;
+
+import utils.jdbcHelper.JdbcUtils;
+
+public class ProcessDao implements ProcessService {
+
+	private JdbcUtils jdbcUtils;
+	public ProcessDao() {
+		// TODO Auto-generated constructor stub
+		jdbcUtils = new JdbcUtils();
+	}
+
+	public boolean login(List<Object> params) {
+		// TODO Auto-generated method stub
+		boolean flag = false;
+		
+		try {
+			jdbcUtils.getConnection();
+			String sql = "select * from user where name = ? and password = ?";
+			Map<String, Object> map = jdbcUtils.findSimpleResult(sql, params);
+			flag = !map.isEmpty()?true:false;			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally{
+			
+		//¹Ø±ÕÊý¾Ý¿â
+		jdbcUtils.releaseConn();
+			
+		}
+		
+		return flag;
+	}	
+
+}
---
+++ allCodeHere/webproject/src/main/java/main/process/service/ProcessService.java
@@ -0,0 +1,8 @@
+package main.process.service;
+
+import java.util.List;
+
+public interface ProcessService {
+
+	public boolean login(List<Object> params);
+}
